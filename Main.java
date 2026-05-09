49125962fee194ee86efeb5147807ff5b6c4329e
McXhanas <114591136+McXhanas@users.noreply.github.com>
2026-05-09 15:12:11 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-    
-        GameView view = new GameView();
-        GameController controller = new GameController(view);
-        controller.run();
-    }
-}
